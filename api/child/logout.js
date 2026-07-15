db798d39dc9e1a9952e986d7db7e4341caf77763
DylanB28 <contactmightyminds@gmail.com>
2026-07-15 20:35:46 +0100

    Delete api/child/logout.js

--- api/child/logout.js
+++
@@ -1,7 +0,0 @@
-import { clearChildSessionCookie, requireMethod, sendJson } from '../_lib/server.js';
-
-export default async function handler(req, res) {
-  if (!requireMethod(req, res, 'POST')) return;
-  clearChildSessionCookie(res);
-  return sendJson(res, 200, { success: true });
-}
